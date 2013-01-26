bffdf5599d1ef0f94e1019a4b29b49a4032f45bb
EJIqpEP <ejiqpep@gmail.com>
2013-01-26 10:54:40 +0200

    Added port in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require "rvm/capistrano"
 
 server "78.47.43.228", :web, :app, :db, primary: true
 
-set :post, 222
+set :post, '222'
 set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :system
 set :application, "elcoding"
